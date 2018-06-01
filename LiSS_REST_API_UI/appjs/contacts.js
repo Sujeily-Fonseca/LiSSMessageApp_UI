e62204c91c09f21ecdaec9f66c034a166a7ab81f
liannesr <lianne.sanchez@upr.edu>
2018-06-01 11:43:41 -0400

    contacts

--- LiSS_REST_API_UI/appjs/contacts.js
+++ LiSS_REST_API_UI/appjs/contacts.js
@@ -1,7 +1,7 @@
 angular.module('AppChat').controller('ContactsController', ['$http', '$log', '$scope', '$location', '$routeParams',
     function($http, $log,$scope, $location, $routeParams) {
         var thisCtrl = this;
-
+        var phone ="";
         this.contactList = [];
        this.userId = localStorage.userID;
         //this.userID = localStorage.getItem("UID")
@@ -28,6 +28,46 @@ angular.module('AppChat').controller('ContactsController', ['$http', '$log', '$s
               };
 
           });
+        };
+
+        this.addContact = function(){
+            //var ngroup = thisCtrl.newGroup;
+//            // Need to figure out who I am
+//            if (!this.isValidPhone(this.phone)) {
+//                console.log("Invalid characters in phone number");
+//                return;
+//            }
+            $log.log("CONTACT.")
+            var data = {};
+            data.phone = this.phone;
+
+            var reqURL = "http://localhost:5000/MessageApp/contacts/" + this.userId;
+            console.log("reqURL: " + reqURL);
+            // configuration headers for HTTP request
+            var config = {
+                headers : {
+                    'Content-Type': 'application/json;charset=utf-8;'
+                     //Access-Control-Allow-Headers: Content-Type;
+                     //Access-Control-Allow-Methods: GET, POST, OPTIONS;
+                     //Access-Control-Allow-Origin: *
+                    //'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+                }
+            }
+            // Now issue the http request to the rest API
+            $http.post(reqURL, data, config).then( function (response) {
+                    $log.log("data: " + JSON.stringify(response.data));
+                    // tira un mensaje en un alert
+                    //alert("New user added with id: " + response.data.Part.uid);
+                    $location.path('/contacts');
+                }).catch(function(error){
+                        console.log(error);
+                        alert(error);
+
+
+            });
+
+            //thisCtrl.groupList.unshift({"first_name": author, "last_name" : lname, "message" : msg, "like" : 0, "nolike" : 0});
+            //thisCtrl.newGroup = "";
         };
 
         this.loadContacts();
